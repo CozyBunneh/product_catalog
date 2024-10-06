b8c24246651465eef3508eaca8066bc187c15ed0
Julia Mattjus <julia.mattjus@gmail.com>
2024-10-06 21:21:48 +0200

    frontend e2e test placeholders

---
+++ prodct_fe/src/e2e/product.e2e.test.ts
@@ -0,0 +1,33 @@
+import puppeteer, { Browser, executablePath, Page } from "puppeteer-core";
+import "core-js";
+import { TextEncoder, TextDecoder } from 'util'
+global.TextEncoder = TextEncoder
+// @ts-expect-error
+global.TextDecoder = TextDecoder
+
+
+describe("Product Tests", () => {
+  let browser: Browser;
+  let page: Page;
+
+  const baseUrl = "http://localhost:3000";  // Adjust if running on a different port
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: true,
+      devtools: true,
+      args: [
+        '--disable-web-security',
+        '--disable-features=IsolateOrigins',
+        '--disable-site-isolation-trials'
+      ],
+      executablePath: "/usr/bin/chromium",
+    });
+    page = await browser.newPage();
+    await page.goto(baseUrl);  // Navigate to the app before running any tests
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  })
+});
